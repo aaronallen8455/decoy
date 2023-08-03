6cf5a6af9b932713ea48767fa7b2dd73acdf1a6a
Aaron Allen <aaronallen8455@gmail.com>
2023-08-03 00:07:50 -0500

    can add rules

---
+++ README.md
@@ -0,0 +1,9 @@
+# decoy :duck:
+
+## What?
+Run a server that returns mock responses for use in testing. New response
+configuration can be added dynamically so that test data needn't exist solely
+in static files. Responses use mustache templating with access to variables
+from JSON request bodies, URL parameters, and query string arguments.
+
+_Work in progress_
--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import           Control.Applicative (asum)
 import           Control.Concurrent.MVar
 import           Control.Monad
 import qualified Data.Aeson as Aeson
-import qualified Data.ByteString as BS
+import qualified Data.Aeson.Types as Aeson
+import           Data.Bifunctor (first)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Lazy.Char8 as BS8
 import qualified Data.Map.Strict as M
@@ -19,38 +21,46 @@ import qualified Text.Mustache as Stache
 
 main :: IO ()
 main = do
-  initRules <- newMVar [testRule]
+  initRules <- newMVar []
   Warp.runEnv 9000 (app initRules)
-
-testRule :: Rule
-testRule = MkRule
-  { pathRule = [Static "one", PathParam "two"]
-  , queryRule = mempty
-  , response = either (error . show) id $
-      Stache.compileTemplate "" "{\"hello\": \"world\", \"bodyParam\":\"{{body.two}}\", \"pathParam\":\"{{path.two}}\"}"
-  }
 
 app :: Rules -> Wai.Application
 app rulesMVar req respHandler = do
   let reqPath = Wai.pathInfo req
-      queryMap = M.fromList $ Wai.queryString req
+      queryMap = M.mapKeys TE.decodeUtf8Lenient
+               . fmap (fmap TE.decodeUtf8Lenient)
+               . M.fromList
+               $ Wai.queryString req
   reqBodyBS <- Wai.strictRequestBody req
   let eReqBodyJson =
         case lookup Http.hContentType $ Wai.requestHeaders req of
           Just ct | ct `elem` ["text/json", "application/json"] ->
             Just <$> Aeson.eitherDecode reqBodyBS
           _ -> Right Nothing
-  rules <- readMVar rulesMVar
-  respHandler $
-    case eReqBodyJson of
-      Left err -> Wai.responseLBS Http.badRequest400 []
-                    $ "Invalid JSON: " <> BS8.pack err
-      Right mReqJson ->
-        case asum $ matchRule queryMap reqPath mReqJson <$> rules of
-          Nothing -> Wai.responseLBS Http.notFound404 [] "No rule matched"
-          Just resp ->
-            Wai.responseLBS Http.ok200 []
-                         (LBS.fromStrict $ TE.encodeUtf8 resp)
+
+  case reqPath of
+    ["_rules"] ->
+      case compileRule =<< Aeson.parseEither Aeson.parseJSON
+                       =<< maybe (Left "No body") Right
+                       =<< eReqBodyJson of
+        Left err -> respHandler
+                  . Wai.responseLBS Http.badRequest400 []
+                  $ "Invalid JSON: " <> BS8.pack err
+        Right rule -> do
+          modifyMVar_ rulesMVar (pure . (rule :))
+          respHandler $ Wai.responseLBS Http.ok200 [] "Rule added"
+    _ -> do
+      rules <- readMVar rulesMVar
+      respHandler $
+        case eReqBodyJson of
+          Left err -> Wai.responseLBS Http.badRequest400 []
+                        $ "Invalid JSON: " <> BS8.pack err
+          Right mReqJson ->
+            case asum $ matchRule queryMap reqPath mReqJson <$> rules of
+              Nothing -> Wai.responseLBS Http.notFound404 [] "No rule matched"
+              Just resp ->
+                Wai.responseLBS Http.ok200 []
+                             (LBS.fromStrict $ TE.encodeUtf8 resp)
 
 data Rule = MkRule
   { pathRule :: [Path]
@@ -60,13 +70,51 @@ data Rule = MkRule
 
 type Rules = MVar [Rule]
 
-type QueryParams = M.Map BS.ByteString (Maybe BS.ByteString)
+type QueryParams = M.Map T.Text (Maybe T.Text)
 
 data Path
   = Static T.Text
   | PathParam T.Text
 
-type QueryRules = M.Map BS.ByteString (Maybe BS.ByteString) -- TODO ignore vs require no value
+pathFromText :: T.Text -> [Path]
+pathFromText txt = parsePart <$> T.split (== '/') txt
+  where
+    parsePart p =
+      case T.stripPrefix ":" p of
+        Nothing -> Static p
+        Just r -> PathParam r
+
+pathToText :: [Path] -> T.Text
+pathToText = T.intercalate "/" . map partToText
+  where
+    partToText = \case
+      Static t -> t
+      PathParam t -> ":" <> t
+
+data RuleRequest = MkRuleRequest
+  { reqPath :: T.Text
+  , reqQueryRules :: Maybe QueryRules
+  , reqTemplate :: T.Text
+  }
+
+compileRule :: RuleRequest -> Either String Rule
+compileRule req = do
+  template <- first show . Stache.compileTemplate ""
+            $ reqTemplate req
+  Right MkRule
+    { pathRule = pathFromText $ reqPath req
+    , queryRule = fromMaybe M.empty $ reqQueryRules req
+    , response = template
+    }
+
+instance Aeson.FromJSON RuleRequest where
+  parseJSON = Aeson.withObject "Rule Request" $ \o ->
+    MkRuleRequest
+    <$> o Aeson..: "path"
+    <*> o Aeson..:? "query"
+    <*> o Aeson..: "response"
+
+type QueryRules = M.Map T.Text (Maybe T.Text) -- TODO ignore vs require no value
 type PathArgs = M.Map T.Text T.Text
 
 matchPath :: [Path] -> [T.Text] -> Maybe PathArgs
@@ -103,7 +151,6 @@ renderTemplate pathArgs queryMap mReqJson template =
   where
     vars = M.fromList
       [ ("path" :: T.Text, Aeson.toJSON pathArgs)
-      , ("query", Aeson.toJSON . M.mapKeys TE.decodeUtf8Lenient
-                    $ M.mapMaybe (fmap TE.decodeUtf8Lenient) queryMap)
+      , ("query", Aeson.toJSON queryMap)
       , ("body", fromMaybe Aeson.Null mReqJson)
       ]
